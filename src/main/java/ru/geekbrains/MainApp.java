073e0fa4484006e3b6bf4ddba6ba22e6c5ec5656
Saiev Magomed <77410732+saievmaga@users.noreply.github.com>
2021-05-04 21:07:09 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/MainApp.java
@@ -0,0 +1,20 @@
+package ru.geekbrains;
+
+import sun.net.www.protocol.http.logging.HttpLogFormatter;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.logging.*;
+
+public class MainApp {
+
+
+    public static void main(String[] args){
+
+
+
+
+    }
+}
